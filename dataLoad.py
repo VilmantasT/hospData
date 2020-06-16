a1d6ad61d0c386573212942b1c26511f13a98d72
VilmantasT <spalviskis@gmail.com>
2020-06-16 16:50:34 +0300

    second commit

--- dataLoad.py
+++ dataLoad.py
@@ -17,6 +17,8 @@ cur.executescript('''
     
     CREATE TABLE IF NOT EXISTS paslaugos (id INTEGER NOT NULL UNIQUE AUTOINCREMENT, pasl_kodas INTEGER, pasl_pavadinimas TEXT)
     
-    CREATE TABLE IF NOT EXISTS gydytojai (id INTEGER NOT NULL UNIQUE AUTOINCREMENT, spaudo_nr NUMERIC UNIQUE, vardas TEXT, pavarde TEXT)
+    CREATE TABLE IF NOT EXISTS gydytojai (id INTEGER NOT NULL UNIQUE AUTOINCREMENT, spaudo_nr NUMERIC UNIQUET, pavarde TEXT)
+    
+    CREATE TABLE IF NOT EXISTS ziniarastis (id INTEGER NOT NULL UNIQUE AUTOINCREMENT, paslauga TEXT, specialistas TEXT, viso_apsilan INTEGER, viso_123_be_N INTEGER, del_ligos_L INTEGER, profilak_Pr INTEGER, mokami_is_viso INTEGER, mokami_is_ju_Pr INTEGER, kons_viso INTEGER, kons_be_siun INTEGER, kons_del_disp INTEGER, kons_but_pag INTEGER)
                   ''')
                   
