8391ee6357a2123c148cb8b9ec656849bb132d86
Vilmantas Tamulenas <vilmantas1990@gmail.com>
2020-07-16 00:03:32 +0300

    json dump file

--- dumpDataToJson.py
+++ dumpDataToJson.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import sqlite3
+
+conn = sqlite3.connect('hospitalData.sqlite')
+
+cur = conn.cursor()
+
 def dumpData(conn, cur):
-    
+
+    ziniarastis = cur.execute('SELECT paslaugos.pasl_kodas, gydytojai.spaudo_nr || " " || gydytojai.pavarde, ziniarastis.kons_viso, ziniarastis.data FROM ziniarastis INNER JOIN gydytojai ON gydytojai.id = ziniarastis.specialistas INNER JOIN paslaugos ON paslaugos.id = ziniarastis.paslauga')
+
+    fhand = open('jsonData.js', 'w')
+
+    for row in ziniarastis:
+        print(row)
+
+dumpData(conn, cur)
