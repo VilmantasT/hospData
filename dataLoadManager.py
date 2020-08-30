f7557a2005f00232b1c747c073c9bfc06388f9c6
Vilmantas Tamulenas <vilmantas1990@gmail.com>
2020-08-30 19:27:14 +0300

    added 1 print statement to dataload manager

--- dataLoadManager.py
+++ dataLoadManager.py
@@ -15,7 +15,7 @@ import loadDRG
 
 def dataLoader(conn, cur):
     print("From what source you want to upload data?")
-    raportChoice = input("Enter 1 - Upload from Ziniarastis; 2 - Upload from Detali; 3 - Upload from ambulatory Suvestine; 4 - Uload from stationary Suvestine")
+    raportChoice = input("Enter 1 - Upload from Ziniarastis; 2 - Upload from Detali; 3 - Upload from ambulatory Suvestine; 4 - Uload from stationary Suvestine; 5 - Upload from DRG report")
 
     if raportChoice == '1':
         newsChoice = input("Enter 1 - Upload newsRaport with temporary data; 2 - Upload newsRaport with permanent data ")
